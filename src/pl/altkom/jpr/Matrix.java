7bb22766a885b6856a469cc683d9ba0b72bc8f3e
Administrator <Administrator@10.27.32.140>
2015-05-09 10:22:03 +0200

    init

---
+++ src/pl/altkom/jpr/Matrix.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.altkom.jpr;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Matrix {
+    
+    protected double var[][];
+
+    public Matrix(int rows, int columns) {
+        var = new double[rows][columns];
+    }
+    
+    public void setValue(int r, int c, double value) {
+        var[r][c] = value;
+    }
+    
+    public double getValue(int r, int c) {
+        return var[r][c];
+    }
+    
+    
+    
+}
---
+++ test/pl/altkom/jpr/MatrixTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.altkom.jpr;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MatrixTest {
+    
+    public MatrixTest() {
+    }
+
+    /**
+     * Test of setValue method, of class Matrix.
+     */
+    @Test
+    public void testSetValue() {
+        
+        Matrix matrix = new Matrix(3, 3);
+        
+        matrix.setValue(1, 1, 12);
+        
+        assert matrix.var[1][1] == 12 ;
+        
+    }
+    
+    @Test
+    public void testSetValueError() {
+        
+        Matrix matrix = new Matrix(3, 3);
+        
+        matrix.setValue(1, 1, 12);
+        
+        assert !(matrix.var[1][1] == 1);
+        
+    }
+
+    /**
+     * Test of getValue method, of class Matrix.
+     */
+    @Test
+    public void testGetValue() {
+        
+        Matrix matrix = new Matrix(3, 3);
+        
+        matrix.var[0][0] = 11;
+        
+        assert matrix.getValue(0, 0) == 11;
+        
+    }
+    
+}

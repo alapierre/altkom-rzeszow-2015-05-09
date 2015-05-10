c16a51dcae012289cc8dfe51d5f365d4450eb9b6
Administrator <Administrator@10.27.32.29>
2015-05-10 11:33:14 +0200

    Invoice & InvoiceItem -> Update 3

--- src/pl/altkom/jpr/bookstore/Invoice.java
+++ src/pl/altkom/jpr/bookstore/Invoice.java
@@ -20,13 +20,111 @@ package pl.altkom.jpr.bookstore;
  * @author Szczepan <szczeixs@gmail.com>
  */
 public class Invoice {
-    String buyer;
-    String seller;
-    int invoiceID;
-    double tax;
-    String paymentMethonName;
-    String delivieryMethodName;
-    String invoiceDate;
+    private String buyer;
+    private String seller;
+    private int invoiceID;
+    private double tax;
+    private String paymentMethonName;
+    private String delivieryMethodName;
+    private String invoiceDate;
+
+    /**
+     * @return the buyer
+     */
+    public String getBuyer() {
+        return buyer;
+    }
+
+    /**
+     * @param buyer the buyer to set
+     */
+    public void setBuyer(String buyer) {
+        this.buyer = buyer;
+    }
+
+    /**
+     * @return the seller
+     */
+    public String getSeller() {
+        return seller;
+    }
+
+    /**
+     * @param seller the seller to set
+     */
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    /**
+     * @return the invoiceID
+     */
+    public int getInvoiceID() {
+        return invoiceID;
+    }
+
+    /**
+     * @param invoiceID the invoiceID to set
+     */
+    public void setInvoiceID(int invoiceID) {
+        this.invoiceID = invoiceID;
+    }
+
+    /**
+     * @return the tax
+     */
+    public double getTax() {
+        return tax;
+    }
+
+    /**
+     * @param tax the tax to set
+     */
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    /**
+     * @return the paymentMethonName
+     */
+    public String getPaymentMethonName() {
+        return paymentMethonName;
+    }
+
+    /**
+     * @param paymentMethonName the paymentMethonName to set
+     */
+    public void setPaymentMethonName(String paymentMethonName) {
+        this.paymentMethonName = paymentMethonName;
+    }
+
+    /**
+     * @return the delivieryMethodName
+     */
+    public String getDelivieryMethodName() {
+        return delivieryMethodName;
+    }
+
+    /**
+     * @param delivieryMethodName the delivieryMethodName to set
+     */
+    public void setDelivieryMethodName(String delivieryMethodName) {
+        this.delivieryMethodName = delivieryMethodName;
+    }
+
+    /**
+     * @return the invoiceDate
+     */
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    /**
+     * @param invoiceDate the invoiceDate to set
+     */
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
     
     
 }
